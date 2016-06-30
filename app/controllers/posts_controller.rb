d2692d4608a65316127d8b6a67d57a271cd886f1
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2016-06-30 20:31:57 +0000

    Added post_controller and testing functions

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,18 @@
+class PostsController < ApplicationController
+  def index
+  end
+  
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+  
+end
